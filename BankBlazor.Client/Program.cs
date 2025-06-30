e4201796d60ea41c89748feda41d08059dd8f39e
Gabriel Uifalean <gabriel13ro@gmail.com>
2025-06-30 14:43:50 +0200

    no ideea...

--- BankBlazor.Client/Program.cs
+++ BankBlazor.Client/Program.cs
@@ -1,19 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BankBlazor.Client;
-namespace BankBlazor.Client;
 
-        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 // Koppla <App> till <div id="app"> i index.html
-        builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("#app");
 
-// LÃ¤gg ocksÃ¥ till HeadOutlet sÃ¥ att <PageTitle> fungerar
+// Lägg också till HeadOutlet så att <PageTitle> fungerar
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Peka HttpClient mot ditt API pÃ¥ rÃ¤tt port (och inte mot klientens egen 5001)
+// Peka HttpClient mot ditt API på rätt port (och inte mot klientens egen 5001)
 builder.Services.AddScoped(sp =>
     new HttpClient { BaseAddress = new Uri("https://localhost:7252/") }
 );
 
-        await builder.Build().RunAsync();
+await builder.Build().RunAsync();
