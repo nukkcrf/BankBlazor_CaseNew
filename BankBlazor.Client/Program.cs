6bc52d06100bac0caa02efd7c5bd6b3429254131
Gabriel Uifalean <gabriel13ro@gmail.com>
2025-06-30 14:43:24 +0200

    Push error?

--- BankBlazor.Client/Program.cs
+++ BankBlazor.Client/Program.cs
@@ -3,15 +3,17 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BankBlazor.Client;
 namespace BankBlazor.Client;
 
-public class Program
-{
-    public static async Task Main(string[] args)
-    {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
-        builder.RootComponents.Add<App>("#app");
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001/") });
 
+// Koppla <App> till <div id="app"> i index.html
+        builder.RootComponents.Add<App>("#app");
+
+// Lägg också till HeadOutlet så att <PageTitle> fungerar
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Peka HttpClient mot ditt API på rätt port (och inte mot klientens egen 5001)
+builder.Services.AddScoped(sp =>
+    new HttpClient { BaseAddress = new Uri("https://localhost:7252/") }
+);
 
         await builder.Build().RunAsync();
-    }
-}
